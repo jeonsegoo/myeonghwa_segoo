39cccf848067fa1b00ce9de6875a1b1de6c3eb6f
jeonsegoo <59784899+jeonsegoo@users.noreply.github.com>
2022-04-21 22:50:26 +0900

    fetch
    fetch

---
+++ airflow/send_email.py
@@ -0,0 +1,9 @@
+# -*- coding : utf-8 -*-
+
+import sys
+import logging
+import io
+from airflow.models import DAG
+from airflow.models import Variable
+from airflow.utils.trigger_rule import TriggerRule
+
